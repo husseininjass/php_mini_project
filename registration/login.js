2e1fcbfa915d2e955bbf9b51c274141862695b9a
husseininjass <hussein.husam7@gmail.com>
2023-08-15 13:41:31 +0300

    project

--- registration/login.js
+++ registration/login.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', function(e) {
     const email = document.querySelector('.email').value;
     const password = document.querySelector('.password').value;
     const wraning = document.querySelector('.text');
-    let status = false;
+   
    
     fetch('http://localhost/php_mini_project/registration/fetch.php', {
         method: 'POST',
@@ -18,14 +18,19 @@ form.addEventListener('submit', function(e) {
     .then(function(data){
         data.forEach(function(e,i){
             if(data[i].email === email && data[i].password === password){
-                status = true;
+                if(email === "admin@admin.com" ){
+                    window.location.href = "../view_page/welcome.html";
+                }
+                else{
+                    window.location.href = "../view_page/user.html";
+                }
                 
-                window.location.href = "../view_page/welcome.html";
+            }
+            else{
+                wraning.textContent = "invalid email or password";
             }
         })
-        if(status === false){
-            wraning.textContent = "invalid email or password";
-        }
+        
 
     })
     
